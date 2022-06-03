2b35700846d6e5ec757e9aa5a4eab6272d4fa8cf
Jose Gustavo Martins <jgustavomartins@gmail.com>
2022-06-03 10:58:41 +0100

    primeiro

---
+++ start.R
@@ -0,0 +1,9 @@
+### instalar hugo
+# blogdown::install_hugo()
+# ultima versao v0.100.1
+
+### criar site com theme papermod
+# blogdown::new_site(theme = "adityatelange/hugo-PaperMod")
+# http://localhost:3648
+# para parar o server blogdown::stop_server() 
+
